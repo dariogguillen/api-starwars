eb1c5b6d55eccf21201010aa96f023ed3c8aa6eb
dariogguillen <dar_fe@hotmail.com>
2018-01-10 13:12:13 -0600

    Se crea seccion de seleccion de personaje y se agrega carpeta img

--- js/main.js
+++ js/main.js
@@ -1,25 +1,39 @@
 const request = new XMLHttpRequest();
 const container = document.querySelector('#container');
-//let containerDiv = document.createElement('div');
+const personaje = document.querySelector('#personaje');
 
-let images =[
-    'https://vignette.wikia.nocookie.net/starwars/images/d/d9/Luke-rotjpromo.jpg/revision/latest/scale-to-width-down/350?cb=20091030151422',
-    'https://images-na.ssl-images-amazon.com/images/I/814YRV0Y8ML._SY679_.jpg',
-    'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/5863/5863603_sd.jpg;maxHeight=640;maxWidth=550',
-    'http://pictures.ozy.com/pictures/900x900/2/9/8/92298_darthvaderf.jpg',
-    'http://3.bp.blogspot.com/--Oxg0f8Ypoo/VXG-vx1wrxI/AAAAAAAAC1M/zC9hJO88rQI/s1600/leia-princess-leia-organa-solo-skywalker-9301324-449-661.jpg',
-    'https://vignette.wikia.nocookie.net/headhuntersholosuite/images/5/5e/Owen_Lars_001.jpg/revision/latest?cb=20100305172555',
-    'https://vignette.wikia.nocookie.net/headhuntersholosuite/images/1/17/Beru_Lars_001.jpg/revision/latest?cb=20100317024358',
-    'http://darksidetoy.com/image/cache/data/1%20BY%20BRAND/SIDESHOW/R5%20D4%20Sixth%20Scale%20Figure/F-700x700.jpg',
-    'https://vignette.wikia.nocookie.net/swfanon/images/5/57/BiggsDarklighter.jpg/revision/latest?cb=20120324222406',
-    'https://nerdist.com/wp-content/uploads/2017/12/download.jpg'
+const images =[
+    '../img/luke.jpg',
+    '../img/C3PO.jpg',
+    '../img/r2-d2.jpg',
+    '../img/Darth Vader.jpeg',
+    '../img/Leia Organa.jpeg',
+    '../img/Owen Lars.jpg',
+    '../img/Beru Whitesun lars.webp',
+    '../img/R5-D4.jpg',
+    '../img/Biggs Darklighter.webp',
+    '../img/Obi-Wan Kenobi.jpg'
 ];
 
+const person = {
+    person0: 'Luke_Skywalker',
+    person2: 'R2_D2',
+    person1: 'C_3PO',
+    person3: 'Darth_Vader',
+    person4: 'Leia_Organa',
+    person5: 'Owen_Lars',
+    person6: 'Beru_Whitesun_Lars',
+    person7: 'R5_D4',
+    person8: 'Biggs_Darklighter',
+    person9: 'Obi_Wan_Kenobi',
+    person10: 'Todos'
+};
+
+
 const setCard = (json) =>{
-    console.log(json.results);
-
-    for (let i = 0; i < json.results.length; i++) {
-
+    
+    const card = (json, i) => {
+        
         let card = `
         <div class='carta card${[i]}'>
             <div class='info'>
@@ -38,14 +52,67 @@ const setCard = (json) =>{
         containerDiv.setAttribute('class', 'card')
         container.appendChild(containerDiv);
         containerDiv.innerHTML = card;
+        
     }
+    
+    const removeCard = () => {
+        const selectCard = document.querySelectorAll('.card');
+        let long = selectCard.length;
+        for (let i = 0; i < long; i++) {
+            
+            container.removeChild(selectCard[i]);
+            
+        }
+    }
+
+    if (personaje.value == person.person10 || personaje.value == undefined) {
+        removeCard();
+        let long = json.results.length
+        for (let i = 0; i < long; i++) {
+            card(json, i)
+        }
+    } else if ( personaje.value == person.person0 ) {
+        removeCard();
+        card(json, 0);
+    } else if (personaje.value == person.person1) {
+        removeCard();
+        card(json, 1);
+    } else if (personaje.value == person.person2) {
+        removeCard();
+        card(json, 2);
+    } else if (personaje.value == person.person3) {
+        removeCard();
+        card(json, 3);
+    } else if (personaje.value == person.person4) {
+        removeCard();
+        card(json, 4);
+    } else if (personaje.value == person.person5) {
+        removeCard();
+        card(json, 5);
+    } else if (personaje.value == person.person6) {
+        removeCard();
+        card(json, 6);
+    } else if (personaje.value == person.person7) {
+        removeCard();
+        card(json, 7);
+    } else if (personaje.value == person.person8) {
+        removeCard();
+        card(json, 8);
+    } else if (personaje.value == person.person9) {
+        removeCard();
+        card(json, 9);
+    }
+    
+    console.log(personaje.value)
+    // changeValue(card);
 }
 
 const star = () => {
     if (request.status == 200) {
         console.log('esta bien');
         let json = JSON.parse(request.responseText);
-        setCard(json)
+        setCard(json);
+
     } else {
         console.log('no esta bien');
     }
@@ -59,3 +126,5 @@ request.onload = star;
 request.onerror = starerror;
 request.open('GET', 'https://swapi.co/api/people/');
 request.send();
+
+personaje.addEventListener('change', star);
